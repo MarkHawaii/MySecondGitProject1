af9440b29a96796911ed5720b75a0a08199eb1a2
mekangurbanov88 <mekangurbanov88@gmail.com>
2018-06-12 00:12:11 -0700

    Updated classes

--- src/main/GoodEveningClass.java
+++ src/main/GoodEveningClass.java
@@ -1,5 +1,7 @@
 package main;
-
+/*
+ * Maria class lesson19
+ */
 public class GoodEveningClass {
 	public static void main(String[] args) {
 		System.out.println("Good Evening class !!!");
--- src/main/GoodmorningClass.java
+++ src/main/GoodmorningClass.java
@@ -1,5 +1,7 @@
 package main;
-
+/*
+ * Maria class lesson19
+ */
 public class GoodmorningClass {
 	public static void main(String[] args) {
 		System.out.println("GoodMorning class !");
